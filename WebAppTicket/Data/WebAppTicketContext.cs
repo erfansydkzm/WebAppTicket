e12d3b29f35b9069fd4fd6444d97cc31aac40fe2
erfansydkzm <seyedkazemierfan@yahoo.com>
2022-01-03 09:53:01 +0330

    Add User Entity And Context Class And Ef&Identity Package.

--- WebAppTicket/Data/WebAppTicketContext.cs
+++ WebAppTicket/Data/WebAppTicketContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebAppTicket.Data
 {
-    public class TicketInfo
+    public class WebAppTicketContext
     {
 
     }
---
+++ WebAppTicket/Data/WebAppTicketDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTicket.Data
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
